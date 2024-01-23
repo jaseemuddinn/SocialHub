f26dd7487128a18bb17ddbc90b7d5a012a860995
jaseemuddin <jaseemuddinn@gmail.com>
2024-01-24 02:09:37 +0530

    bug fix

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -93,7 +93,7 @@ function Signup({ id = 'M2jMt3Dy' }) {
                                                 <label for="Name" className="block text-sm mb-2 dark:text-white">Name</label>
                                                 <div className="relative">
                                                     <input type="name" id="displayName" name="name" onChange={(e) => setName(e.target.value)} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="name-error" />
-                                                    <div className="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                                                    <div className="invisible absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                                                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                                                         </svg>
@@ -105,7 +105,7 @@ function Signup({ id = 'M2jMt3Dy' }) {
                                                 <label for="email" className="block text-sm mb-2 dark:text-white">Email address</label>
                                                 <div className="relative">
                                                     <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error" />
-                                                    <div className="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                                                    <div className="invisible absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                                                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                                                         </svg>
@@ -118,7 +118,7 @@ function Signup({ id = 'M2jMt3Dy' }) {
                                                 <label for="password" className="block text-sm mb-2 dark:text-white">Password</label>
                                                 <div className="relative">
                                                     <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="password-error" />
-                                                    <div className="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                                                    <div className="invisible absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                                                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                                                         </svg>
@@ -131,7 +131,7 @@ function Signup({ id = 'M2jMt3Dy' }) {
                                                 <label for="confirm-password" className="block text-sm mb-2 dark:text-white">Confirm Password</label>
                                                 <div className="relative">
                                                     <input type="password" id="confirm-password" name="confirm-password" onChange={(e) => setConfirmPassword(e.target.value)} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="confirm-password-error" />
-                                                    <div className="hidden absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
+                                                    <div className="invisible absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                                                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                                                         </svg>
