95a5eb283b6ff3664205d8fac44debd36ccd7c29
jaseemuddin <jaseemuddinn@gmail.com>
2024-02-25 03:14:00 +0530

    URI Updated

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
 
-    const uri = process.env.URI;
+  const uri = URI = "mongodb+srv://jaseem:8qgqcISk1lQB4Z8Z@cluster0.sl1lqjy.mongodb.net/?retryWrites=true&w=majority";
 
-    try {
-        // Connect the client to the server
-        const connect = await mongoose.connect(uri, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        });
+  try {
+    // Connect the client to the server
+    const connect = await mongoose.connect(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
-        console.log('MongoDb Connected to server', connect.connection.host);
+    console.log('MongoDb Connected to server', connect.connection.host);
 
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 
 
 
